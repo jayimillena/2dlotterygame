fd0e2ff25c288e5754137c01c918144281a17808
Two <22344029+jayimillena@users.noreply.github.com>
2023-09-26 10:35:29 +0800

    Modified form content

--- index.php
+++ index.php
@@ -3,9 +3,13 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>2D Lottery</title>
 </head>
 <body>
-  
+  <form action="lottery.php" method="POST">
+    <input type="text" name="txt-num1" placeholder="Choose number from 1-32" />
+    <input type="text" name="txt-num2" placeholder="Choose number from 1-32" />
+    <input type="submit" name="btn-play" value="Play" />
+  </form>
 </body>
 </html>
